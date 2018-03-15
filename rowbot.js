2ac21c916430fb1e23daff23eb629a5c21f118b7
Jacqueline <jacqueline@Jacqueline-Macbook-Pro.local>
2018-03-15 19:34:18 -0400

    Undo Redo functionality added

--- rowbot.js
+++ rowbot.js
@@ -1,3 +1,68 @@
+class MAction {
+    constructor(type, matrix, row, values) {
+        this.type = type;
+        this.matrix = matrix;
+        this.row = row;
+        this.values = values;
+    }
+}
+
+let undoStack = [];
+let redoStack = [];
+
+function undo() {
+    if (undoStack.length <= 0) {
+        return;
+    }
+    const lastAction = undoStack.pop();
+
+    if (lastAction.type == 0) { // Normal type
+        let redoAction = new MAction(lastAction.type, lastAction.matrix, lastAction.row, lastAction.matrix.content[lastAction.row]);
+        redoStack.push(redoAction);
+
+        lastAction.matrix.content[lastAction.row] = lastAction.values;
+    }
+    else if (lastAction.type == 1) { // Swap type
+        let redoAction = new MAction(lastAction.type, lastAction.matrix, lastAction.row, lastAction.values);
+        redoStack.push(redoAction);
+
+        let temp = [];
+        for (let i = 0; i < lastAction.matrix.column; i++) {
+            temp[i] = lastAction.matrix.content[lastAction.values.to][i];
+            lastAction.matrix.content[lastAction.values.to][i] = lastAction.matrix.content[lastAction.values.from][i];
+            lastAction.matrix.content[lastAction.values.from][i] = temp[i];
+        }
+    }
+
+}
+
+function redo() {
+    if (redoStack.length <= 0) {
+        return;
+    }
+    const lastAction = redoStack.pop();
+
+    if (lastAction.type == 0) { // Normal type
+        let redoAction = new MAction(lastAction.matrix, lastAction.row, lastAction.matrix.content[lastAction.row]);
+        undoStack.push(redoAction);
+
+        lastAction.matrix.content[lastAction.row] = lastAction.values;
+    }
+    else if (lastAction.type == 1) { // Swap type
+        let to = lastAction.values.to;
+        let from = lastAction.values.from;
+        let redoAction = new MAction(lastAction.matrix, lastAction.row, {to, from});
+        undoStack.push(redoAction);
+
+        let temp = [];
+        for (let i = 0; i < lastAction.matrix.column; i++) {
+            temp[i] = lastAction.matrix.content[lastAction.values.to][i];
+            lastAction.matrix.content[lastAction.values.to][i] = lastAction.matrix.content[lastAction.values.from][i];
+            lastAction.matrix.content[lastAction.values.from][i] = temp[i];
+        }
+    }
+}
+
 function getGCD(a, b) {
 	return b ? getGCD(b, a % b) : a;
 }
@@ -92,6 +157,9 @@ class Matrix {
     }
     
     swapRow(from, to) {
+        let newAction = new MAction(1, this, to, {from, to});
+        undoStack.push(newAction);
+
         let temp = [];
         for (let i = 0; i < this.column; i++) {
             temp[i] = this.content[to][i];
@@ -101,7 +169,10 @@ class Matrix {
     }
 
     doOperation(to, op, top, bottom) {
+        let previous = [];
+        
         for (let i = 0; i < this.column; i++) {
+            previous[i] = new MNumber(this.content[to][i].top, this.content[to][i].bottom);
             switch (op) {
             case 0: // Add
                 this.content[to][i].add(top, bottom);
@@ -117,10 +188,17 @@ class Matrix {
                 break;
             }
         }
+
+        let newAction = new MAction(0, this, to, previous);
+        undoStack.push(newAction);
+        redoStack = [];
     }
 
     rowOperation(to, op, from) {
+        let previous = [];
+
         for (let i = 0; i < this.column; i++) {
+            previous[i] = new MNumber(this.content[to][i].top, this.content[to][i].bottom);
             switch (op) {
             case 0: // Add
                 this.content[to][i].add(this.content[from][i].top, this.content[from][i].bottom);
@@ -136,6 +214,10 @@ class Matrix {
                 break;
             }
         }
+
+        let newAction = new MAction(0, this, to, previous);
+        undoStack.push(newAction);
+        redoStack = [];
     }
 }
 
